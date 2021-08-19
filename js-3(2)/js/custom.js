6eeaadd9b5276beb6aed1b29a3417810cc229e95
annapetriga33 <83228999+annapetriga33@users.noreply.github.com>
2021-08-19 19:23:34 +0300

    Update custom.js

--- js-3(2)/js/custom.js
+++ js-3(2)/js/custom.js
@@ -3,9 +3,9 @@ var feets = prompt('Значение в футах');
 var howFeets = feets * 0.305;
 
 if (kilometers > howFeets){
-    alert('Значение в километрах будет больше');
+    alert('Значение в километрах ' + kilometers + ' будет больше чем в футах ' + howFeets);
 }   else if (kilometers < howFeets){
-    alert('Значение в футах будет больше');
+    alert('Значение в футах ' + howFeets + ' будет больше чем в километрах ' + kilometers);
 }   else {
     alert('Они равны');
 }
